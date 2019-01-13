e4918af79593ed16830f2f181e305a9410c1f4aa
Jakub-Syrek <jakubvonsyrek@gmail.com>
2019-01-13 01:22:47 +0100

    Update ScanSubnetInfo.ps1

--- ScanSubnetInfo.ps1
+++ ScanSubnetInfo.ps1
@@ -1,9 +1,7 @@
 $ScriptFol = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
 $stopwatch =  [system.diagnostics.stopwatch]::StartNew()
-
-
-
+Add-Type -AssemblyName PresentationFramework
 function Restart-PowerShell-Elevated
 {
    $Script = $ScriptFol + "\ScanSubnetInfo.ps1"
@@ -114,21 +112,76 @@ for ( [int]$i = $min ; $i -le $max ; $i++ )
  return $Hosts ;
  }
   
+[xml]$XAMLWindow = '
+<Window 
+    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+    Height="Auto"
+    SizeToContent="WidthAndHeight"
+    Title="Get-Info-From-Subnet">
+    <ScrollViewer Padding="10,10,10,0" ScrollViewer.VerticalScrollBarVisibility="Disabled">
+        <StackPanel>
+            <StackPanel Orientation="Horizontal">
+                <Label Margin="10,10,0,10">Subnet:</Label>
+                <TextBox Name="Input" Margin="10" Width="250px"></TextBox>
+            </StackPanel>
+            <DockPanel>
+                <Button Name="ButtonGetService" Content="Get-Info" Margin="10" Width="150px" IsEnabled="false"/>
+                <Button Name="ButtonClose" Content="Close" HorizontalAlignment="Right" Margin="10" Width="50px"/>
+            </DockPanel>
+        </StackPanel> 
+    </ScrollViewer >
+</Window>
+'
+
+# Create the Window Object
+$Reader=(New-Object System.Xml.XmlNodeReader $XAMLWindow)
+$Window=[Windows.Markup.XamlReader]::Load( $Reader )
+
+    # TextChanged Event Handler for Input 
+    $TextboxInput = $Window.FindName("Input")
+    $TextboxInput.add_TextChanged.Invoke({
+    $Network = $TextboxInput.Text
+    $ButtonGetService.IsEnabled = $Hosts -ne ''
+})
+
+    # Click Event Handler for ButtonClose
+    $ButtonClose = $Window.FindName("ButtonClose")
+    $ButtonClose.add_Click.Invoke({
+    $Window.Close();
+})
+
+    # Click Event Handler for ButtonGetService
+    $ButtonGetService = $Window.FindName("ButtonGetService")
+    $ButtonGetService.add_Click.Invoke({
+    $Network = $TextboxInput.text.Trim()
+    try{
+         [array]$Hosts = Build-Source-array "15" "250" ;
+         $Hosts | Start-Parallel -Scriptblock ${Function:\Get-UserMachineInfo} ; 
+         Get-Content $txtPath |  Out-GridView ;
+         Invoke-Item $txtPath ;
+         Write-Host "All threads returned in :" $StopWatch.Elapsed.ToString() ;              
+    }catch{
+        [System.Windows.MessageBox]::Show($_.exception.message,"Error",[System.Windows.MessageBoxButton]::OK,[System.Windows.MessageBoxImage]::Error)
+    }
+})
+
+
+
+  
    Install-Parallel-Execution ;
    $myIPs = Get-My-Ips ;
    $myIP = $myIPs.ipaddress[0] ;
    $IndexOfLastDot = $myIP.lastindexof(".") ;
    $Network = $myIP.substring(0,$IndexOfLastDot) ;
 
-   [array]$Hosts = Build-Source-array "15" "250" ;
+   
    $txtPath = "C:\TMP\tmp.csv" ;
    if ([downloader]::new().CheckIfPathExists($txtPath))
       {Remove-Item $txtPath ;} ;
+   # Open the Window
+   $Window.ShowDialog() | Out-Null
    
-   $Hosts | Start-Parallel -Scriptblock ${Function:\Get-UserMachineInfo} ;
    
-   Get-Content $txtPath |  Out-GridView ;
-   Invoke-Item $txtPath ;
-   Write-Host "All threads returned in :" $StopWatch.Elapsed.ToString() ;
-   Pause ;
+   
  
